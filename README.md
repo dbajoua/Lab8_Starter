71a12732df1a104b91f584328590f183790a4f54
dbajoua <93298088+dbajoua@users.noreply.github.com>
2022-11-20 15:43:57 -0800

    completed screenshots + part 2

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Lab 8 - Starter
-1) You would fit your automated tests within a Github action that runs whenever code is pushed.
-   The purpose of automated tests is for scripts to automatically test new and existing software
-   based on an event occurence like code getting pushed to the repository.
 
-2) You would not use end to end testing becuase... 
+Aleksandra Desens, Delvin Bajoua
+
+Question 1
+Answer: Within a Github action that runs whenever code is pushed.
+Automatically running tests at every push ensures that no faulty code is deployed. If any of the tests fail, application would not be deployed and the last working version would be preserved. The purpose of automated tests is for scripts to automatically test new and existing software.
+
+Question 2
+Answer: No. 
+Testing a single fuction should be handled by unit testing. End to end would be very slow and inefficient, as it tries to replicated the user's actions from beginning to end. Unit testing helps to check individual functions to make sure proper functionality in the code. For instance, this can be used to check proper return value of a function.
+
+Question 3
+Answer: No.
+Testing a feature should be tested by feature testing. Unit testing can test only isolated components of that feature, not the overall process. End to end testing tries to repeat the workflow of the user from the start to the very end, which would serve more appropriate in testing a user writing and sending a message to another user. 
+
+Question 4
+Answer: Yes.
+Checking max message length should be handled by a single function and that can be effectively tested by a unit test. A unit test helps test an individual part of the code, and thus an appropriate tool to ensure max message length quickly and efficently. 
    
    
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// 4 tests for function isPhoneNumber
+test('matches valid phone numbers: 760-222-3344', () => {
+    expect(functions.isPhoneNumber('760-222-3344')).toBeTruthy();
+});
+
+test('matches valid phone numbers: (760) 222-3344', () => {
+    expect(functions.isPhoneNumber('(760) 222-3344')).toBeTruthy();
+});
+
+test('matches valid phone numbers: 760', () => {
+    expect(functions.isPhoneNumber(760)).toBeFalsy();
+});
+
+test('matches valid phone numbers: abc', () => {
+    expect(functions.isPhoneNumber('abc')).toBeFalsy();
+});
+
+// 4 tests for function isEmail
+test('matches valid emails: Apple@ucsd.edu', () => {
+    expect(functions.isEmail('Apple@ucsd.edu')).toBeTruthy();
+});
+
+test('matches valid emails: banana@ucsd.edu', () => {
+    expect(functions.isEmail('banana@ucsd.edu')).toBeTruthy();
+});
+
+test('matches valid emails: 123', () => {
+    expect(functions.isEmail('123')).toBeFalsy();
+});
+
+test('matches valid emails: happy@ucsd', () => {
+    expect(functions.isEmail('happy@ucsd')).toBeFalsy();
+});
+
+// 4 tests for function isStrongPassword
+test('checks for a strong password: Abcd_123', () => {
+    expect(functions.isStrongPassword('Abcd_123')).toBeTruthy();
+});
+
+test('checks for a strong password: Abcdefghijk_123', () => {
+    expect(functions.isStrongPassword('Abcdefghijk_123')).toBeTruthy();
+});
+
+test('checks for a strong password: ____', () => {
+    expect(functions.isStrongPassword('____')).toBeFalsy();
+});
+
+test('checks for a strong password: 12345', () => {
+    expect(functions.isStrongPassword('12345')).toBeFalsy();
+});
+
+// 4 tests for function isDate
+test('checks for a valid date: 1/2/2023', () => {
+    expect(functions.isDate('1/2/2023')).toBeTruthy();
+});
+
+test('checks for a valid date: 11/22/2023', () => {
+    expect(functions.isDate('11/21/2023')).toBeTruthy();
+});
+
+test('checks for a valid date: 11/22/23', () => {
+    expect(functions.isDate('11/22/23')).toBeFalsy();
+});
+
+test('checks for a valid date: 11.22.2023', () => {
+    expect(functions.isDate('11/22/23')).toBeFalsy();
+});
+
+// 4 tests for function isHexCode
+test('matches hex color: 34568B', () => {
+    expect(functions.isHexColor('34568B')).toBeTruthy();
+});
+
+test('matches hex color: 45B8AC', () => {
+    expect(functions.isHexColor('45B8AC')).toBeTruthy();
+});
+
+test('matches hex color: 1-2-3', () => {
+    expect(functions.isHexColor('1-2-3')).toBeFalsy();
+});
+
+test('matches hex color: aaabbbccc', () => {
+    expect(functions.isHexColor('aaabbbccc')).toBeFalsy();
+});
